1e090dd2b528ce01d1d097090eb8714d8bedb83f
Brett Dong <brett.browning.dong@gmail.com>
2019-11-20 13:31:47 +0800

    Revised H

--- training/20-Nov-2019/H.cpp
+++ training/20-Nov-2019/H.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-#include <cstdlib>
+#include <bits/stdc++.h>
 using namespace std;
 const int maxn = 250001;
-int a[maxn], b[maxn], d[maxn], n, pivot;
+long long a[maxn], b[maxn], d[maxn], d1[maxn], d2[maxn], d3[maxn];
+int n, pivot;
 
-long long eval() {
-    long long ans = abs(d[0]);
+long long eval(long long *arr) {
+    long long ans = abs(arr[0]);
     for(int i = 1; i < n; i++)
     {
-        d[i] += d[i-1];
-        ans += abs(d[i]);
+        arr[i] += arr[i-1];
+        ans += abs(arr[i]);
     }
     return ans;
 }
@@ -23,19 +23,27 @@ int main()
         cin >> a[i];
     for(int i = 0; i < n; i++)
         cin >> b[i];
-    for(int i = 0; i < n; i++)
-        d[i] = ((a[i] <= pivot) && (b[i] <= pivot)) - ((a[i] > pivot) && (b[i] > pivot));
-    long long ans = eval();
-    if(n & 1) {
-        for (int i = 0; i < n; i++)
-            d[i] = ((a[i] < pivot) && (b[i] < pivot)) - ((a[i] >= pivot) && (b[i] >= pivot));
-        ans = min(ans, eval());
-        for (int i = 0; i < n; i++)
+    long long ans = 1e15;
+    if(n % 2 == 0) {
+        for (int i = 0; i < n; i++) {
+            d[i] = ((a[i] <= pivot) && (b[i] <= pivot)) - ((a[i] > pivot) && (b[i] > pivot));
+        }
+        ans = eval(d);
+    } else {
+        for (int i = 0; i < n; i++) {
             d[i] = ((a[i] <= pivot) && (b[i] < pivot)) - ((a[i] > pivot) && (b[i] >= pivot));
-        ans = min(ans, eval());
-        for (int i = 0; i < n; i++)
+        }
+        ans = eval(d);
+        for (int i = 0; i < n; i++) {
             d[i] = ((a[i] < pivot) && (b[i] <= pivot)) - ((a[i] >= pivot) && (b[i] > pivot));
-        ans = min(ans, eval());
+        }
+        ans = min(ans, eval(d));
+        for (int i = 0; i < n; i++) {
+            d1[i] = (a[i] == pivot && b[i] > pivot) - (a[i] < pivot && b[i] == pivot);
+            d2[i] = (a[i] > pivot && b[i] > pivot) - (a[i] < pivot && b[i] < pivot);
+            d3[i] = (a[i] > pivot && b[i] == pivot) - (a[i] == pivot && b[i] < pivot);
+        }
+        ans = min(ans, eval(d1) + eval(d2) + eval(d3));
     }
     cout << ans << endl;
     return 0;
