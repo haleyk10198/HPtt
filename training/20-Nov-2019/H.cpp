02b778f2c61e3e6c9aefe5b536363585517ce24f
Brett Dong <brett.browning.dong@gmail.com>
2019-11-20 11:14:23 +0800

    Edited H

--- training/20-Nov-2019/H.cpp
+++ training/20-Nov-2019/H.cpp
@@ -4,22 +4,38 @@ using namespace std;
 const int maxn = 250001;
 int a[maxn], b[maxn], d[maxn], n, pivot;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin >> n;
-    pivot = n / 2;
-    for(int i = 0; i < n; i++)
-        cin >> a[i];
-    for(int i = 0; i < n; i++)
-        cin >> b[i];
-    for(int i = 0; i < n; i++)
-        d[i] = (a[i] < pivot) - (b[i] < pivot);
+long long eval() {
     long long ans = abs(d[0]);
     for(int i = 1; i < n; i++)
     {
         d[i] += d[i-1];
         ans += abs(d[i]);
+    }
+    return ans;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin >> n;
+    pivot = (n >> 1) + (n & 1);
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    for(int i = 0; i < n; i++)
+        cin >> b[i];
+    for(int i = 0; i < n; i++)
+        d[i] = ((a[i] <= pivot) && (b[i] <= pivot)) - ((a[i] > pivot) && (b[i] > pivot));
+    long long ans = eval();
+    if(n & 1) {
+        for (int i = 0; i < n; i++)
+            d[i] = ((a[i] < pivot) && (b[i] < pivot)) - ((a[i] >= pivot) && (b[i] >= pivot));
+        ans = min(ans, eval());
+        for (int i = 0; i < n; i++)
+            d[i] = ((a[i] <= pivot) && (b[i] < pivot)) - ((a[i] > pivot) && (b[i] >= pivot));
+        ans = min(ans, eval());
+        for (int i = 0; i < n; i++)
+            d[i] = ((a[i] < pivot) && (b[i] <= pivot)) - ((a[i] >= pivot) && (b[i] > pivot));
+        ans = min(ans, eval());
     }
     cout << ans << endl;
     return 0;
